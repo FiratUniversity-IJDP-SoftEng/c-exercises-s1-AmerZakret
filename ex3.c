b8fc8b7b371b819c8ef0a2f662ef1bb976046db8
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-16 11:31:38 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char pet[3];
+
+    printf("Do you have a cat or a dog? ");
+    scanf("%s", pet);
+
+    if (strcmp(pet, "cat") == 0) {
+        printf("Your cat says: Meow!\n");
+    } else if (strcmp(pet, "dog") == 0) {
+        printf("Your dog says: Woof!\n");
+    } else {
+        printf("Invalid input\n");
+    }
+}
 
